bf6ec539f1923ce89ded01f54c199fb475fb006d
Loren Oh <loh@kopri.re.kr>
2022-01-04 15:00:01 +0900

    Add zsh func for sdcv on 42

--- config/zsh/deep.zsh
+++ config/zsh/deep.zsh
@@ -13,8 +13,15 @@ export LD_LIBRARY_PATH=$HOME/.local/lib
 alias ls="ls --color=auto"
 alias k="k -ha"
 
+# Conda initialization
 function pyinit() {
     source $HOME/.local/conda/etc/profile.d/conda.sh 
     conda activate
 }
 
+# Webster's dictionary using sdcv
+function d() {
+	sdcv -n --utf8-output --color "$@" 2>&1 | \
+	fold --width=$(tput cols) | \
+	less --quit-if-one-screen -RX
+}
