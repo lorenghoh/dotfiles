84ecc422ba8b321d63140c45403cf31533426949
Loren Oh <loh@kopri.re.kr>
2022-03-16 09:51:57 +0900

    Update Lualine config

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -1,67 +1,67 @@
-require('packer').startup(function()
-    use 'wbthomason/packer.nvim'
+require("packer").startup(function()
+    use "wbthomason/packer.nvim"
 
     use {
-        'vhyrro/neorg',
-        ft = 'norg',
-        branch = 'main',
+        "vhyrro/neorg",
+        ft = "norg",
+        branch = "main",
         config = {
             function()
-                require('config.norg')
+                require("config.norg")
             end
         },
-        after = 'nvim-treesitter',
-        requires = 'nvim-lua/plenary.nvim',
+        after = "nvim-treesitter",
+        requires = "nvim-lua/plenary.nvim",
     }
 
     use {
-        'itchyny/calendar.vim'
+        "itchyny/calendar.vim"
     }
 
     use {
-        'nvim-treesitter/nvim-treesitter',
+        "nvim-treesitter/nvim-treesitter",
         config = {
             function()
-                require('config.treesitter')
+                require("config.treesitter")
             end
         },
         requires = {
-            'nvim-treesitter/playground',
+            "nvim-treesitter/playground",
             opt = true
         },
-        run = ':TSUpdate'
+        run = ":TSUpdate"
     }
 
     use {
-        'neovim/nvim-lspconfig',
+        "neovim/nvim-lspconfig",
         config = { 
             function() 
-                require('config.lsp')
+                require("config.lsp")
             end
         }
     }
 
     use {
-        'hrsh7th/nvim-cmp',
+        "hrsh7th/nvim-cmp",
         config = {
             function()
-                require('config.cmp')
+                require("config.cmp")
             end
         },
         requires = {
-            'hrsh7th/cmp-nvim-lsp',
-            'hrsh7th/cmp-buffer',
-            'hrsh7th/cmp-path',
-            'hrsh7th/cmp-cmdline',
-            'hrsh7th/cmp-vsnip',
-            'hrsh7th/vim-vsnip',
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
+            "hrsh7th/cmp-vsnip",
+            "hrsh7th/vim-vsnip",
         }
     }
 
     use {
-        'L3MON4D3/LuaSnip',
+        "L3MON4D3/LuaSnip",
         requires = {
-            'rafamadriz/friendly-snippets',
+            "rafamadriz/friendly-snippets",
         },
         config = {
             function()
@@ -71,59 +71,57 @@ require('packer').startup(function()
     }
 
     use {
-        'nvim-telescope/telescope.nvim',
+        "nvim-telescope/telescope.nvim",
         config = { 
             function() 
-                require('config.telescope')
+                require("config.telescope")
             end
         },
         requires = {
-            'nvim-lua/popup.nvim',
-            'nvim-lua/plenary.nvim',
+            "nvim-lua/popup.nvim",
+            "nvim-lua/plenary.nvim",
         },
     }
 
     use {
-        'alvarosevilla95/luatab.nvim',
+        "alvarosevilla95/luatab.nvim",
         config = {
             function()
-                require('luatab').setup({})
+                require("luatab").setup({})
             end
         },
         requires = {
-            'kyazdani42/nvim-web-devicons',
+            "kyazdani42/nvim-web-devicons",
             opt = true,
         },
     }
 
     use {
-        'hoob3rt/lualine.nvim',
-        requires = {
-            'kyazdani42/nvim-web-devicons',
-            opt = true,
-        },
+        "hoob3rt/lualine.nvim",
+        after = "github-nvim-theme",
+        config = {
+            function()
+                require("lualine").setup({
+                    options = {
+                        icons_enabled = true,
+                        theme = "auto",
+                        component_separators = {"", ""},
+                        section_separators = {"", ""},
+                        disabled_filetypes = {}
+                    }
+                })
+            end
+        }
     }
 
     use {
-        'projekt0n/github-nvim-theme',
+        "projekt0n/github-nvim-theme",
         config = {
             function()
-                require('github-theme').setup({
-                    theme_style = 'dark',
+                require("github-theme").setup({
+                    theme_style = "dimmed",
                 })
             end
         }
     }
 end)
-
--- lualine
-require('lualine').setup {
-    options = {
-        icons_enabled = true,
-        theme = 'auto',
-        component_separators = {'', ''},
-        section_separators = {'', ''},
-        disabled_filetypes = {}
-    },
-}
-
