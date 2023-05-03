22a69cad67f3b6c1f23a619914aa84f2511121a1
Loren Oh <loh@kopri.re.kr>
2023-05-03 15:17:21 +0900

    Use Vim colorscheme

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -158,7 +158,7 @@ require("packer").startup(function()
         config = {
             function()
                 require("github-theme").setup({
-                    theme_style = "dark",
+                    vim.cmd("colorscheme github_dark_dimmed")
                 })
             end
         }
