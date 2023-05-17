70344fd809c0965c30bb926949f375e84cba75c9
Loren Oh <loh@kopri.re.kr>
2023-05-17 14:12:24 +0900

    Add basic config for wezterm

---
+++ config/wezterm/wezterm.lua
@@ -0,0 +1,17 @@
+local wezterm = require "wezterm"
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- Local config
+config.font = wezterm.font "FiraCode Nerd Font"
+config.font_size = 11
+
+config.color_scheme = 'github_dimmed'
+
+return config
+
