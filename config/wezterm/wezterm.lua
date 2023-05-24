d93b94c7631e9624a44362b6bb2262cf9ccf035d
Loren Oh <loh@kopri.re.kr>
2023-05-24 15:31:52 +0900

    Wezterm: remove bottom padding

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,16 +1,19 @@
-local wezterm = require "wezterm"
+local wezterm = require("wezterm")
 local config = {}
 
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- Local config
-config.font = wezterm.font "FiraCode Nerd Font"
+config.font = wezterm.font("FiraCode Nerd Font")
 
-config.color_scheme = 'github_dimmed'
+config.color_scheme = "github_dimmed"
+
+config.window_padding = {
+	bottom = 0,
+}
 
 return config
-
