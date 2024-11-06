57f89a724c5216f45fee9da9dc7c847a6b2b35f7
Loren Oh <lorenoh@Coraline.local>
2024-11-06 21:05:34 +0900

    Wez: Adjust start-up configs

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,14 +1,16 @@
 local wezterm = require("wezterm")
 local config = {}
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
 if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
 -- Local config
 config.font = wezterm.font("FiraCode Nerd Font")
+config.font_size = 13
+
+config.initial_cols = 96
+config.initial_rows = 54
 
 config.color_scheme = "github_dimmed"
 config.window_padding = {
